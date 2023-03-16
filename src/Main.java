ae218e032bb553fdc43b5202e58e96c4b68f349f
renanmello <mellorenan19@gmail.com>
2023-03-16 18:07:06 -0300

    main class change

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.dio.desafio.dominio.Bootcamp;
 import br.dio.desafio.dominio.Curso;
+import br.dio.desafio.dominio.Dev;
 import br.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -6,24 +8,54 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
+        //criando curso 1
         Curso curso1 = new Curso();
         curso1.setTitulo("java");
         curso1.setDescricao("Curso java 11");
         curso1.setCargaHoraria(8);
-
+        //criando curso 2
         Curso curso2 = new Curso();
         curso2.setTitulo("python");
         curso2.setDescricao("Curso Python");
         curso2.setCargaHoraria(4);
-
+        //criando mentoria
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Java");
         mentoria.setDescricao("mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        //testes
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+
+        //criando bootcamp
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java + Python");
+        bootcamp.setDescricao(" bootcamp POO");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        //criando os devs
+        Dev devrenan = new Dev();
+        devrenan.setNome("renan");
+        devrenan.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteudos inscritos renan: "+devrenan.getConteudosInscritos());
+        System.out.println("Conteudos concluidos renan: "+devrenan.getConteudosConcluidos());
+        System.out.println("XP: "+ devrenan.calcularXp());
+
+        Dev devcami = new Dev();
+        devcami.setNome("camila");
+        devcami.inscreverBootcamp(bootcamp);
+
+        //testando os resultados
+        System.out.println("Conteudos inscritos camila: "+devcami.getConteudosInscritos());
+        System.out.println("Conteudos concluidos camila: "+devcami.getConteudosConcluidos());
+        devcami.progredir();
+        devcami.progredir();
+        System.out.println("XP: "+ devcami.calcularXp());
 
     }
 }
